2a4efe4797c386711da5b8404ac51c8ad993799f
biongoo <biongoo@wp.pl>
2022-05-03 20:17:00 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Main, Rent } from './pages';
+import CssBaseline from '@mui/material/CssBaseline';
+import styles from './App.module.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Car } from './types';
 
-function App() {
-  return (
-    <div className="app">
-      xd
-    </div>
-  );
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const cars: Car[] = [
+  {
+      id: 1,
+      name: 'BMW M5 GTS',
+      pricePerDay: 1200,
+      url: 'https://www.tuningblog.eu/wp-content/uploads/2019/10/Vorsteiner-GTS-Carbon-Bodykit-Tuning-BMW-M5-F90-1.jpg',
+  },
+  {
+      id: 2,
+      name: 'Ferrari 488 Pista',
+      pricePerDay: 2500,
+      url: 'https://cdn.car-recalls.eu/wp-content/uploads/2020/04/Ferrari-488-Pista-2018-recall-fuel-fire.jpg',
+  },
+  {
+      id: 3,
+      name: 'VW Golf 8 R',
+      pricePerDay: 800,
+      url: 'https://img.chceauto.pl/volkswagen/golf/volkswagen-golf-hatchback-5-drzwiowy-4514-50024_head.webp',
+  },
+]
+
+const App = () => {
+    return ( 
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className={styles.app}>
+            <Routes>
+                <Route path="rent/:carId" element={<Rent cars={cars} />} />
+                <Route path="*" element={<Main cars={cars} />} />
+            </Routes>
+          </div>
+        </ThemeProvider>    
+    );
 }
 
 export default App;
---
+++ src/pages/Main.tsx
@@ -0,0 +1,49 @@
+import { Link } from 'react-router-dom';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { Car } from '../types';
+
+export const Main = ({ cars }: { cars: Car[] }) => {
+    const carsOutput = cars.map(car => (
+        <Card sx={{ maxWidth: 250 }} key={car.id}>
+            <CardMedia
+                component="img"
+                alt="green iguana"
+                height="140"
+                image={car.url}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {car.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Price: {car.pricePerDay} PLN
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button component={Link} size="small" to={`/rent/${car.id}`}>
+                    Rent for one day!
+                </Button>
+            </CardActions>
+        </Card>
+    ));
+
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+                justifyContent: 'center',
+                gap: 3,
+            }}
+        >
+            {carsOutput}
+        </Box>
+    );
+};
